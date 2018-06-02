89881218085979ee8f61a8ba6799228042e4aef0
SEVERUS\Vincent <delortvincent@live.fr>
2018-06-02 11:07:26 +0200

    Saving progress

--- ObjectLib.py
+++ ObjectLib.py
@@ -1,3 +1,20 @@
-class Reicipe:
-    def __init__(self, name):       # TODO #
+# -*- coding: UTF-8 -*-
+class Recette:
+    def __init__(self, name, kcal, glucose=0, lipides=0, acides_gras_satures=0, proteines=0):
         self.name = name
+        self.kcal = kcal
+        self.glucose = glucose
+        self.lipides = lipides
+        self.acides_gras_satures = acides_gras_satures
+        self.proteines = proteines
+
+
+class Repas:
+    def __init__(self, date, heure):
+        self.date = date
+        self.heure = heure
+        self.kcal = 0
+        self.glucose = 0
+        self.lipides = 0
+        self.acide_gras_satures = 0
+        self.proteines = 0
---
+++ UILib.py
@@ -0,0 +1,10 @@
+help = '\t- quit\n' \
+       '\t- +recette\n' \
+       '\t- *recette\n' \
+       '\t- +repas\n' \
+       '\t- -repas\n'
+
+
+def cmd():
+    entry = str(raw_input('> '))
+
